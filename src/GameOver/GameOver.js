9e9a5c01c834243401707991c971968ebc2e6da8
Petar Stevovski <stevovski.petar@gmail.com>
2019-02-07 18:13:07 +0100

    Updated text.

--- src/GameOver/GameOver.js
+++ src/GameOver/GameOver.js
@@ -5,7 +5,7 @@ const gameOver = props => {
     return (
         <div className="game-over_menu">
             <h1>GAME OVER</h1>
-            <p>You failed to figure out what the correct word was, and now Bill is dead.</p>
+            <p>You failed to figure out what the correct word was, and now Bill is dead. Don't be like Bill, remember?</p>
             <p>Your score: <span>{props.score}</span></p>
             <div className="game-over_menu-buttons">
                 <button onClick={props.exit}>EXIT</button>
