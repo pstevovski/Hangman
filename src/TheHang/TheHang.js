f1aedccf6cbd447674a79fcec9d448e268946917
Petar Stevovski <stevovski.petar@gmail.com>
2019-02-10 00:13:48 +0100

    Fixed bug which caused some letters to not go in disabled mode.

--- src/TheHang/TheHang.js
+++ src/TheHang/TheHang.js
@@ -3,14 +3,14 @@ import './TheHang.css';
 
 const theHang = props => {
     // DISPLAY THE PARTS AS LESS LIVES ARE AVAILABLE
-    if(props.lives === 6) {
+    if(props.lives === 5) {
         document.querySelectorAll(".parts").forEach(part => part.style.display = "none");
     }
 
     // COUNTER TO MIMIC THE ID OF THE ELEMENT WHOSE STYLE SHOULD CHANGE
-    if(props.counter > 0) {
-        let counter = props.counter;
-        document.getElementById(`${counter}`).style.display = "flex";
+    let counter = props.counter;
+    if(props.counter > 0 && props.counter <= 5) {
+        document.getElementById(`part${counter}`).style.display = "flex";
     }
     
     return (
@@ -20,18 +20,18 @@ const theHang = props => {
             <div className="hang-top">
                 {/* THE MAN THATS BEING HANGED */}
                 <div className="hangman">
-                    <div className="hangman-head parts" id="1">
+                    <div className="hangman-head parts" id="part1">
                         <div className="hangman-eyes">
                             <div className="hangman-eye"></div>
                             <div className="hangman-eye"></div>
                         </div>
                         <div className="hangman-mouth"></div>
                     </div>
-                    <div className="hangman-torso limbs parts" id="2"></div>
-                    <div className="hangman-left_hand limbs parts" id="3"></div>
-                    <div className="hangman-right_hand limbs parts" id="4"></div>
-                    <div className="hangman-left_leg limbs parts" id="5"></div>
-                    <div className="hangman-right_leg limbs parts" id="6"></div>
+                    <div className="hangman-torso limbs parts" id="part2"></div>
+                    <div className="hangman-left_hand limbs parts" id="part3"></div>
+                    <div className="hangman-right_hand limbs parts" id="part4"></div>
+                    <div className="hangman-left_leg limbs parts" id="part5"></div>
+                    <div className="hangman-right_leg limbs parts" id="part6"></div>
                 </div>
             </div>
         </div>
