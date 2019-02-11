72fef38ad95cfed6266b8a24626bb51223f187c1
Petar Stevovski <stevovski.petar@gmail.com>
2019-02-11 16:21:07 +0100

    Added footer and GitHub repo link in main menu.

--- src/MainMenu/MainMenu.js
+++ src/MainMenu/MainMenu.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './MainMenu.css';
+import logo from '../github-logo.png'
 
 const mainMenu = props => {
     return (
         <div className="main-menu">
             <h1>The Hangman</h1>
             <button onClick={props.play}>Play</button>
-            {/* <button>About</button> */}
+            <a href="https://github.com/pecko95/Hangman"><img src={logo} width="24px" height="24px" alt="GitHub" title="GitHub repository" /></a>
+
+            <footer>
+                <p>Created with <span>React</span>. Favicon from <a href="https://www.iconfinder.com/icons/190319/game_hangman_icon">IconFinder</a></p>
+            </footer>
         </div>
     )
 }
