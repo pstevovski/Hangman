b49de9904934d528c7e5d8d3bb933869edf91fca
Petar Stevovski <stevovski.petar@gmail.com>
2019-02-11 17:06:25 +0100

    Included build folder.

---
+++ build/precache-manifest.aac8119ce6ed3d93d72dd8c75307cf89.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c88cecbffad6d8e731fd95de49561ebd",
+    "url": "/static/media/Montserrat-SemiBold.c88cecbf.ttf"
+  },
+  {
+    "revision": "a98626e1aef6ceba5dfc1ee7112e235a",
+    "url": "/static/media/Montserrat-Medium.a98626e1.ttf"
+  },
+  {
+    "revision": "6d1796a9f798ced8961baf3c79f894b6",
+    "url": "/static/media/Montserrat-Black.6d1796a9.ttf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0f6d5d10caae4bcd5014",
+    "url": "/static/js/main.0f6d5d10.chunk.js"
+  },
+  {
+    "revision": "169aae9a895245b56704",
+    "url": "/static/js/1.169aae9a.chunk.js"
+  },
+  {
+    "revision": "0f6d5d10caae4bcd5014",
+    "url": "/static/css/main.768089e1.chunk.css"
+  },
+  {
+    "revision": "b392c05f35ffe72d5e7254cb6717d809",
+    "url": "/index.html"
+  }
+];
