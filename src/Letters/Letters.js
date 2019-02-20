9ceb725ccad319d98d87211bcbd24fad09c1fa4d
Petar Stevovski <stevovski.petar@gmail.com>
2019-02-20 09:25:21 +0100

    Rendering buttons based on value / empty button for space.

--- src/Letters/Letters.js
+++ src/Letters/Letters.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './Letters.css';
+import spaceBar from './space-bar.svg';
 
 const letters = props => {
+    let letter = null;
+    if(props.letter === " ") {
+        letter = (
+            <img src={spaceBar} alt="Space Bar" width="18px" height="18px" />
+        )
+    } else {
+        letter = props.letter
+    }
     return (
     <div>
-        <button className="letters" id={props.id} onClick={props.click}>{props.letter}</button>
+        <button className="letters" id={props.id} onClick={props.click}>{letter}</button>
     </div>
     )
 }
